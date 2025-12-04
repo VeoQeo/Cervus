90019736a6c3a267dab84c7e7242ec14adec7745
wienton <k13775638@gmail.com>
2025-12-04 21:02:08 +0300

    [CREATE]: create file for config build.wien in main directory

--- builder/src/main.c
+++ builder/src/main.c
@@ -2,7 +2,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int open_file(const char* filename) {
+    if(filename) {perror("file failed open!\n"); return -1;}
+
+    goto read_file;
+
+read_file:
+    printf("sucess read file!");
+}
 
 int main(int argc, char* argv[]) {
+    const char* filename = "build.wien";
+
+    open_file(filename);
+
     printf("builder success started!");
 }
